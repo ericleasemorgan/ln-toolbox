c298ad53f52d323e47e8ddc84a85269451be6e2c
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-10-21 07:19:51 -0400

    Second initial commit

---
+++ bin/results2files.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# results2files.sh - given a Lexis/Nexis Web Services API JSON, cache entry values to files
+
+# Eric Lease Morgan <emorgan@nd.edu>
+# (c) University of Notre Dame; distributed under a GNU Public License
+
+# October 21, 2019 - first documentation
+
+
+# configure
+JSON='./results.json'
+CORPUS='./corpus'
+
+# initialize and make sane
+mkdir -p $CORPUS
+INDEX=0
+
+# process each item in the result
+cat $JSON | jq .value[].Document.Content | while read ENTRY; do
+
+	# remove leading and trailing quote marks; cool!
+	ENTRY="${ENTRY:1}"
+	ENTRY="${ENTRY%?}"
+	
+	# increment and build output filename
+	INDEX=$(( INDEX+1 ))
+	FILE="$CORPUS/entry-$( printf '%03d' $INDEX ).xml"
+	
+	# output
+	echo $ENTRY > $FILE
+	
+# fini
+done
+exit
